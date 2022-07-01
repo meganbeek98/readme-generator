5710b54996e697d099d3b22abd448516a55766ea
meganbeek98 <meganbeek98@gmail.com>
2022-07-01 12:05:47 -0400

    Update generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,65 +1,85 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  
-  if(license === 'MIT'){
-    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  if (license.length === 0) {
+    return ""
+  } else if (license == "Apache") {
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  } else if (license == "Mozilla") {
+    return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+  } else if (license == "MIT") {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+  } else if (license == "GNU") {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+  } else if (license == "Boost") {
+    return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`
+  } else if (license == "ISC") {
+    return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`
   }
-  if(license === 'Mozilla' ){
-      return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
-  }
-  if(license === 'Apache'){
-      return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-  }
-
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  function renderLicenseLink(license) {
-    if(license === 'MIT'){
-        return '[MIT](https://opensource.org/licenses/MIT)'
-    }
-    if(license === 'Mozilla' ){
-        return '[Mozilla](https://opensource.org/licenses/MPL-2.0)'
-    }
-    if(license === 'Apache'){
-        return '[Apache](https://opensource.org/licenses/Apache-2.0)'
-    }
-    if(license === 'none') {
-      return '[none]'
-    }
-
+  if (license.length === 0) {
+    return ""
+  } else if (license == "Apache") {
+    return `https://opensource.org/licenses/Apache-2.0`
+  } else if (license == "Mozilla") {
+    return `https://opensource.org/licenses/MPL-2.0`
+  } else if (license == "MIT") {
+    return `https://opensource.org/licenses/MIT`
+  } else if (license == "GNU") {
+    return `https://www.gnu.org/licenses/gpl-3.0`
+  } else if (license == "Boost") {
+    return `https://www.boost.org/LICENSE_1_0.txt`
+  } else if (license == "ISC") {
+    return `https://opensource.org/licenses/ISC`
+  }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-
-  if(license === 'none'){
-  return ``
-  }
-  else{
-      return`
-      ##${license} 
-      ${renderLicenseBadge(license)}
-      ${renderLicenseLink(license)}
-      `
+  if (license.length === 0) {
+    return ""
+  } else {
+    return `## License
+  ${renderLicenseBadge(license)}
+  
+  ${renderLicenseLink(license)} 
+    `
   }
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+  ## Description
+  ${data.projectDescription}
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  ## Installation
+  ${data.installInfo}
+  ## Usage
+  ${data.usageInfo}
   ${renderLicenseSection(data.license)}
-  #Table of Contents
-- [DESCRIPTION](#Description)
-- [INSTALLATION](#Installation)
-- [USAGE INFO](#Usage info)
-- [CONTRIBUTION GUIDELINES](#Contribution Guidelines)
-- [TESTING](#Testing)
-- [GITHUB LINK](#Github)
-- [EMAIL ME](#Email)
+  ## Contributing
+  
+  ${data.contributionsInfo}
+  
+  ## Tests
+  ${data.testsInfo}
+  ## Questions
+  My GitHub: [${data.githubUsername}](https://github.com/${data.githubUsername}) <br>
+  Email me: ${data.emailInfo}
+`
+}
 
 module.exports = generateMarkdown;
